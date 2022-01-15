00a5c871089ad79fb67b819e4e2b2344b5279ecc
AliyaAM <aliya.amirova005@gmail.com>
2022-01-15 17:20:41 +0000

    deleted testing file

--- testing_quant_only_delete_after.R
+++
@@ -1,3 +0,0 @@
-source('/Users/aliya/my_docs/proj/bayesian_meta_analysis/QuantOnly_Sourcing_AllSeeds.R')
-
-#once the above was done, the results for each seed should be saved in files within the sourced scripts above
