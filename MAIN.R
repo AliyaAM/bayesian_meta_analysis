5450704a2d2c83d9d76ac9446c34c2ab87c20b7f
AliyaAM <aliya.amirova005@gmail.com>
2022-01-15 23:42:37 +0000

    Set local OUTPUT_ROOT

--- MAIN.R
+++ MAIN.R
@@ -3,7 +3,8 @@ library(filenamer)
 
 ## Set the root directory to look for source code.
 SOURCE_ROOT = "/Users/aliya/my_docs/proj/bayesian_meta_analysis/"
-
+## Set the root location on the user's local machine to save output files.
+OUTPUT_ROOT = "/Users/aliya/my_docs/proj/bayesian_meta_analysis/"
 
 #run Bayesian meta-analysis sourcing from 10 different seeds separately for two procedures: 
 
@@ -11,12 +12,11 @@ SOURCE_ROOT = "/Users/aliya/my_docs/proj/bayesian_meta_analysis/"
 source(paste(SOURCE_ROOT, "QualANDQuant_sourcing_AllSeeds.R", sep=""))
 
 #2) updating hyperprior with quantitative evidence only 
-source('/Users/aliya/my_docs/proj/bayesian_meta_analysis/QuantOnly_Sourcing_AllSeeds.R')
+source(paste(SOURCE_ROOT, "QuantOnly_Sourcing_AllSeeds.R", sep=""))
 
 #once the above was done, the results for each seed should be saved in files within the sourced scripts above
 
 #reading from this files average over the results from different seeds below: 
-source('/Users/aliya/my_docs/proj/bayesian_meta_analysis/Mean_overSeeds_QUANT_QUAL.R')
-
-source('/Users/aliya/my_docs/proj/bayesian_meta_analysis/Mean_overSeeds_QUANT.R')
+source(paste(SOURCE_ROOT, "Mean_overSeeds_QUANT_QUAL.R", sep=""))
+source(paste(SOURCE_ROOT, "Mean_overSeeds_QUANT.R", sep=""))
 
