7cec9cfcf76d73a6e8603443e04351eb274e78c7
AliyaAM <aliya.amirova005@gmail.com>
2022-01-16 01:14:26 +0000

    Bayesian MA of both qual and quant (changed how graphs are saved)
    fixed the following error:
    
    Error in file.copy(from = plots.png.paths, to = x_directory) :
      more 'from' files than 'to' files

--- BayesianMetaAnalysis_StepByStep.R
+++ BayesianMetaAnalysis_StepByStep.R
@@ -120,7 +120,7 @@ plots.png.paths <- list.files(plots.dir.path, pattern=".png", full.names = TRUE)
 # save the averaged over seeds results in the directory below: 
 #x_directory <- file.path("/Users/aliya/my_docs/proj/bayesian_meta_analysis", logName)
 
-x_directory <- file.path(paste(OUTPUT_ROOT, logName, "/Results_BayesianMeta_Analysis.csv", sep=""))
+x_directory <- file.path(paste(OUTPUT_ROOT, logName, sep=""))
 
 file.copy(from=plots.png.paths, to=x_directory)
 
