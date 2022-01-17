f2480adc1fc92692ee0029f27807937276b58303
AliyaAM <aliya.amirova005@gmail.com>
2022-01-17 01:55:52 +0000

    Update TEST_DELETE_AFTER.R
    testign new Bayes update so we can avoid using contingencies table sampler
    see the commit below for the commented lines. Old code was commented

--- TEST_DELETE_AFTER.R
+++ TEST_DELETE_AFTER.R
@@ -1,1 +1,8 @@
 #TESTING NEW BAYEs update fUNCTION 
+
+source(paste(SOURCE_ROOT, "BayesUpdateStepByStep.R", sep=""), local = TRUE) # this function (BayesUpdateStepByStep) runs the Bayesian meta-analysis that combines qualitative and quantitative evidence 
+
+seed = as.integer(Sys.time())
+
+Results_Age = BayesUpdateStepByStep(x =x, Construct = "Age", uncertainty = 10, seed = seed)
+Results_BayesianMeta_Analysis = rbind(Results_Age)
