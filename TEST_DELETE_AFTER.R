a918456fd5b3b488bb6e6201131efd4ba442bfbe
AliyaAM <aliya.amirova005@gmail.com>
2022-01-17 02:03:23 +0000

    Update TEST_DELETE_AFTER.R
    changing how the probability is calculated

--- TEST_DELETE_AFTER.R
+++ TEST_DELETE_AFTER.R
@@ -1,5 +1,12 @@
 #TESTING NEW BAYEs update fUNCTION 
 
+
+
+## Set the root directory to look for source code.
+SOURCE_ROOT = "/Users/aliya/my_docs/proj/bayesian_meta_analysis/"
+## Set the root location on the user's local machine to save output files.
+OUTPUT_ROOT = "/Users/aliya/my_docs/proj/bayesian_meta_analysis/"
+
 source(paste(SOURCE_ROOT, "BayesUpdateStepByStep.R", sep=""), local = TRUE) # this function (BayesUpdateStepByStep) runs the Bayesian meta-analysis that combines qualitative and quantitative evidence 
 
 seed = as.integer(Sys.time())
