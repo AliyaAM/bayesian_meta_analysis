4f70a482ec9f6209b4ed5063b8edf6b3c5825f81
AliyaAM <aliya.amirova005@gmail.com>
2022-01-25 22:31:52 +0000

    Update MAIN.R
    #run Bayesian meta-analysis sourcing from 10 different seeds separately for two procedures and then averaging was deleted because we are no longer doing that  as we do not need to sample N_PA_X anymore now that we are plotting a distribution for the log OR

--- MAIN.R
+++ MAIN.R
@@ -35,20 +35,6 @@ x = read.csv(paste(SOURCE_ROOT, "input.csv", sep=""))  #to perform the analysis 
 data = read.csv(paste(SOURCE_ROOT, "QuantData_CheckedForAccuracy_20March2020.csv", sep=""))  #data extracted from from the quantitative studies 
 JaarsmaInternationalStudy = read.csv(paste(SOURCE_ROOT, "HyperPriorData.csv", sep="")) #empirical hyperprior 
 
-#run Bayesian meta-analysis sourcing from 10 different seeds separately for two procedures: 
-
-#1) combining qualitative and quantitative evidence: 
-
-source(paste(SOURCE_ROOT, "QualANDQuant_sourcing_AllSeeds.R", sep=""))
-
-#2) updating hyperprior with quantitative evidence only 
-source(paste(SOURCE_ROOT, "QuantOnly_Sourcing_AllSeeds.R", sep=""))
-
-#once the above was done, the results for each seed should be saved in files within the sourced scripts above
-
-#reading from this files average over the results from different seeds below: 
-source(paste(SOURCE_ROOT, "Mean_overSeeds_QUANT_QUAL.R", sep=""))
-source(paste(SOURCE_ROOT, "Mean_overSeeds_QUANT.R", sep=""))
 
 #plot the findings of the anlaysis of quantitative evidence (i.e., hyperprior was updated with quantitative evidence)
 source(paste(SOURCE_ROOT, "Bayesian_MA_plotting_quantitative_findings.R", sep=""))
