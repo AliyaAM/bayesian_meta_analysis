04bdccf63ee80b424e009e8d6af9b7337d7528b2
AliyaAM <aliya.amirova005@gmail.com>
2023-06-07 19:50:47 +0100

    Create Comparing_KLD_ChatGPTvsHuman.R

---
+++ Comparing_KLD_ChatGPTvsHuman.R
@@ -0,0 +1,10 @@
+
+
+
+read.csv("/Users/aliya/my_docs/proj/bayesian_review_methods/RESULTS/ChatGPT/KLD_ChatGPT.csv")
+
+read.csv("/Users/aliya/my_docs/proj/bayesian_review_methods/RESULTS/Human/KLD_human.csv")
+
+~/my_docs/proj/bayesian_meta_analysis/Comparing_KLD_ChatGPTvsHuman.R
+~/my_docs/proj/bayesian_meta_analysis/KL_divergence_ChatGPT.R
+
